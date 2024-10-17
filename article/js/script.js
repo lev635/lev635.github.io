de2c5a32f6095f4f084a2405d3d34ffeb7818131
lev635 <ryux635@gmail.com>
2024-10-17 21:20:19 +0900

    フォント、2

--- article/js/script.js
+++ article/js/script.js
@@ -4,3 +4,20 @@ fetch("../include/header.html")
 fetch("../include/footer.html")
 .then((response) => response.text())
 .then((data) => document.querySelector("#footer").innerHTML = data);
+
+const last = new Date(document.lastModified);
+const year = last.getFullYear();
+const month = last.getMonth() + 1;
+const date = last.getDate();
+let fixedMonth = month;
+let fixedDate = date;
+if (month < 10) {
+    fixedMonth = "0" + month;
+}
+if (date < 10) {
+    fixedDate = "0" + date;
+}
+const datetimeText = year + "-" + fixedMonth + "-" + fixedDate;
+const target = document.getElementById('last_modified');
+target.textContent = datetimeText;
+target.setAttribute("datetime", datetimeText);
